b6f8a18c840f8c433d86ca768161dc8db7d7c532
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2024-09-11 01:23:05 +0530

    Update 02_scripts.js

--- 02_scripts.js
+++ 02_scripts.js
@@ -1,8 +1,8 @@
 // 1. Array in JS.
 let arrNum   = [ 11,14,15,20,25,30];
-let arrStr   = ['Avi','rishu','Ananya','Bulbul', 'Samiksha' ,"Diksha","Bhaskar"];
+let arrStr   = ['Avi','rishu','Ananya', 'Samiksha' ,"Diksha","Bhaskar"];
 let arrBool  = ['True','False','True','False','True','False'];
-let arrList  = ['Avi',21,'Priya',16,'Rishu',24,'Ananya',22];
+let arrList  = ['Avi','Boy','Priya','girl','Rishu','Bad boy','Ananya','Girl'];
 console.log(arrNum);
 console.log(arrStr);
 console.log(arrBool);
@@ -40,7 +40,7 @@ console.log(arrStr);
 console.log(arrBool);
 console.log(arrList);
 
-
+// 
 // 3. deletion in Array.
 console.log(arrStr);
 
@@ -72,12 +72,18 @@ let filtered  = arrNum.filter((value) =>{
 
 // 6. map function .
 
+let filter = arrNum.filter(value => value>=0)
+let item = filter.map(function(num){
+    return {value:num};
+});
+console.log(item);
+
 // 7. arrow function.
 let filtering  = arrStr.filter((value) =>{
     if(value.includes('Ananya'))
     console.log(value);
         
-})
+// })
 // 8. sorting in array.
 arrList.sort()
 console.log(arrList);
@@ -116,12 +122,37 @@ console.log(arrList);
 
 //11. join two arrays.
 
+const joined = arrNum.join[',']
+console.log(joined);
+
 //12. spread operator.
+let combined = [...arrStr,...arrList];
+console.log("Combined Array",combined);
+
+Mking a copy of combined using spread operator.
+let cpy = [...combined];
+console.log("copy of combined array is created..",cpy);
+
+
 
 //13. Reducing an array.
-//14. chaining in array
+    let sum =  arrNum.reduce((accumulator,currentValue)=>
+        accumulator+currentValue,0
+);
+    console.log(sum)
+   
+    
+//14. chaining in array.
+// let items = arrNum.filter(value => value >=0 ).map(num =>{value:num});
+//     console.log(items);
+let items = arrNum.filter(value => value >=0 ).map(num =>{value:num});
+
+
 
 //15. inbuild method in Array  for data manupulation.
+// let a = arrNum.Math.round()
+// console.log(a);
+
 
 // Inbuilt-Object String:-String is a primitive type as well as  inbuilt object type it depends on syntax so Various method in string is 
 // used to manupulate data whenever required .
@@ -129,6 +160,22 @@ console.log(arrList);
 //String obj manipulation
 
 let Newstring = new String("Avi")
+
+console.log(Newstring.includes('A'));
+console.log(Newstring.length);
+console.log(Newstring.startsWith('A'));
+console.log(Newstring.endsWith('i'));
+console.log(Newstring.slice(1,2));
+console.log(Newstring.indexOf('A'));
+console.log(Newstring.toUpperCase());
+
+console.log(Newstring.toLowerCase());
+
+console.log(Newstring.trim());
+console.log(Newstring.replace('Avi','Rishu'));
+
+
+
 
 //date object
 
@@ -146,6 +193,12 @@ console.log(mydates.getDay()+1);
 console.log(mydates.getFullYear());
 console.log(mydates.getMilliseconds());
 console.log(mydates.getTime());
+
+    
+
+
+
+
 
 
 
